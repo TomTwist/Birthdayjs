9fc00dcee6fd4c0a62877dba7a68d00fcdbf7053
TomTwist <thomas.rudel@hotmail.com>
2018-11-18 17:43:20 +0100

    added the function

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 now = new Date
-theYear=now.getYear()
+var theYear=now.getYear()
 if (theYear < 1900)
 theYear=theYear+1900
+// parseInt(theYear)
 
-var yearOfBirth = prompt("What's the year of your birth?");
+var yearOfBirth = parseInt(prompt("What's the year of your birth?"))
+var counterAge = 0
+
+while ( yearOfBirth <= theYear) {
+  document.write("<p>" + counterAge + " in " + yearOfBirth + "</p>")
+  yearOfBirth += 1
+  counterAge += 1
+}
