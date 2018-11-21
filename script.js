0f80f68a12237a8339f9e219aa748a45f62ba74b
TomTwist <thomas.rudel@hotmail.com>
2018-11-21 13:33:50 +0100

    single quotes

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var yearOfBirth = parseInt(prompt("What's the year of your birth?"))
 var counterAge = 0
 
 while ( yearOfBirth <= theYear) {
-  document.write("<p>" + counterAge + " in " + yearOfBirth + "</p>")
+  document.write("<p class='year-item'>" + counterAge + " in " + yearOfBirth + "</p>")
   yearOfBirth += 1
   counterAge += 1
 }
