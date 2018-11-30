b52e0dd0b3439f7eae7aab80f443068dbc6a5ab8
TomTwist <thomas.rudel@hotmail.com>
2018-11-30 12:21:06 +0100

    replaced the document.write with a get ElementById method

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-now = new Date
-var theYear=now.getYear()
+now = new Date // importiert das heutige Datum
+var theYear = now.getYear() //holt das Jahr aus dem Datum
 if (theYear < 1900)
-theYear=theYear+1900
-// parseInt(theYear)
+theYear= theYear + 1900
+var html = [];
 
 var yearOfBirth = parseInt(prompt("What's the year of your birth?"))
 var counterAge = 0
 
-while ( yearOfBirth <= theYear) {
-  document.write('<p class="year-item">' + counterAge + ' in ' + yearOfBirth + '</p>')
-  yearOfBirth += 1
-  counterAge += 1
+function printAges(message) {
+  var outputDiv = document.getElementById('age-output');
+  outputDiv.innerHTML = message;
 }
+
+
+while (yearOfBirth <= theYear) {
+  html += '<p class="year-item">' + counterAge + ' in ' + yearOfBirth + '</p>'; //hier werden die einzelnen jahre gesammelt und gelistet
+  yearOfBirth += 1;
+  counterAge += 1;
+}
+
+printAges(html);
